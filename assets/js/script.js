1de07577c0213e91a33bcf7b507d6524e6db16a8
faghuba <techaghuba@gmail.con>
2021-03-10 08:49:47 -0800

    final Build

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.
 const IMG_URL = 'https://image.tmdb.org/t/p/w1280'
 const SERACH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3a1d44f0ca0d9af05931a9a936c95ca4&query="'
 
-
+const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
 
@@ -16,8 +16,42 @@ async function getMovies(url) {
     const res = await fetch(url)
     const data = await res.json()
 
-    console.log(data.results);
-}   
+    showMovies(data.results);
+}  
+
+function showMovies(movies) {
+    main.innerHTML = ''
+
+    movies.forEach((movie) => {
+        const { title, poster_path, vote_average, overview } = movie
+        
+        const movieEl = document.createElement('div')
+        movieEl.classList.add('movie')
+
+        movieEl.innerHTML = `
+            <img src="${IMG_URL + poster_path}" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3> 
+                    ${overview} 
+            </div>
+        `
+        main.appendChild(movieEl)
+    })
+}
+
+function getClassByRate(vote) {
+    if(vote >= 8) {
+        return 'green'
+    } else if (vote >= 5) {
+        return 'orange'
+    } else {
+        return 'red'
+    }
+}
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
